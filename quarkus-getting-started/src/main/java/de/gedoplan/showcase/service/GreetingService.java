ad7c75aea9ea3ee836a9e79144dabd609f9a65e0
Dirk Weil <dirk.weil@gedoplan.de>
2019-08-15 15:53:46 +0200

    Demo getting-started übernommen

---
+++ quarkus-getting-started/src/main/java/de/gedoplan/showcase/service/GreetingService.java
@@ -0,0 +1,12 @@
+package de.gedoplan.showcase.service;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+  public String greeting(String name) {
+    return "hello " + name + "\n";
+  }
+
+}
