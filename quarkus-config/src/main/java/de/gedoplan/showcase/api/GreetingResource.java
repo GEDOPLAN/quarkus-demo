a6af72c651ff91ce43004107d704771e1037187f
Dirk Weil <dirk.weil@gedoplan.de>
2019-08-16 11:02:19 +0200

    Demo für Config ergänzt

---
+++ quarkus-config/src/main/java/de/gedoplan/showcase/api/GreetingResource.java
@@ -0,0 +1,33 @@
+package de.gedoplan.showcase.api;
+
+import java.util.Optional;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/hello")
+public class GreetingResource {
+
+  @Inject
+  @ConfigProperty(name = "greeting.message")
+  String message;
+
+  @Inject
+  @ConfigProperty(name = "greeting.suffix", defaultValue = "!")
+  String suffix;
+
+  @Inject
+  @ConfigProperty(name = "greeting.name")
+  Optional<String> name;
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String hello() {
+    return this.message + " " + this.name.orElse("world") + this.suffix;
+  }
+}
---
+++ quarkus-config/src/test/java/de/gedoplan/showcase/NativequarkusIT.java
@@ -0,0 +1,9 @@
+package de.gedoplan.showcase;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativequarkusIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
