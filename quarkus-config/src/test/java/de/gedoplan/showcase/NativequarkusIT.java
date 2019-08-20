50662a2e7cac5e4777df8dd0c08d18e60c4b8370
Dirk Weil <dirk.weil@gedoplan.de>
2019-08-20 10:28:15 +0200

    Kein Native-Test

--- quarkus-config/src/test/java/de/gedoplan/showcase/NativequarkusIT.java
+++
@@ -1,9 +0,0 @@
-package de.gedoplan.showcase;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativequarkusIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
