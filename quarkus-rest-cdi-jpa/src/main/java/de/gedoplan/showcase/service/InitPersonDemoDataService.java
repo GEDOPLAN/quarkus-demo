29c9b928cbdf47c793cd8305cb775a8e75236660
Dirk Weil <dirk.weil@gedoplan.de>
2019-10-29 08:59:08 +0100

    Fix startup event

--- quarkus-rest-cdi-jpa/src/main/java/de/gedoplan/showcase/service/InitPersonDemoDataService.java
+++ quarkus-rest-cdi-jpa/src/main/java/de/gedoplan/showcase/service/InitPersonDemoDataService.java
@@ -1,44 +1,42 @@
 package de.gedoplan.showcase.service;
 
 import de.gedoplan.showcase.entity.Person;
-import de.gedoplan.showcase.persistence.PersonRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import io.quarkus.runtime.StartupEvent;
 
 @ApplicationScoped
 public class InitPersonDemoDataService {
   @Inject
-  PersonRepository personRepository;
+  EntityManager entityManager;
 
-  @Inject
-  Log log;
+  private static Log log = LogFactory.getLog(InitPersonDemoDataService.class);
 
   /**
    * Create test/demo data.
    * Attn: Interceptors may not be called, if method is private!
    *
-   * @param event
-   *          Application scope initialization event
+   * @param event Application scope initialization event
    */
   @Transactional
   void createDemoData(@Observes StartupEvent event) {
     try {
-      if (personRepository.countAll() == 0) {
-        log.debug("Create demo data");
-
-        personRepository.persist(new Person("Duck", "Dagobert"));
-        personRepository.persist(new Person("Duck", "Donald"));
+      if (this.entityManager.createQuery("select count(x) from Person x", Long.class).getSingleResult() == 0) {
+        this.entityManager.persist(new Person("Duck", "Dagobert"));
+        this.entityManager.persist(new Person("Duck", "Donald"));
       }
     } catch (Exception e) {
       log.warn("Cannot create demo data", e);
     }
 
   }
+
 }
