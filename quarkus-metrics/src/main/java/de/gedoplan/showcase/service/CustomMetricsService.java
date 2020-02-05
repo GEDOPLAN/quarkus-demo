75984d07796b697fdb6971de4368180a6addd006
Dirk Weil <dirk.weil@gedoplan.de>
2020-02-05 16:50:15 +0100

    .

--- quarkus-metrics/src/main/java/de/gedoplan/showcase/service/CustomMetricsService.java
+++ quarkus-metrics/src/main/java/de/gedoplan/showcase/service/CustomMetricsService.java
@@ -18,7 +18,7 @@ public class CustomMetricsService {
   /*
    * Metrics collection via @Gauge (and @Timed, @Counted, ...) is done by CDI interceptors and happens therefore only,
    * if bean instances have been created. This demo 'service' isn't used anywhere else, leaving it uninstantiated.
-   * Therefore we cheat a little bit by observing the application scope inititlization event, forcing bean instantiation then.
+   * Therefore we cheat a little bit by observing the application scope initialization event, forcing bean instantiation then.
    */
   void startUp(@Observes @Initialized(ApplicationScoped.class) Object object) {
   }
