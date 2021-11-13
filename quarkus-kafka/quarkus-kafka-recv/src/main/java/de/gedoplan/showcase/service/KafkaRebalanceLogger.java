577299d405a6293b5e64a0a123b3fbb687e0f5f6
Dirk Weil <dirk.weil@gedoplan.de>
2021-11-13 19:41:03 +0100

    Doku aktualisiert; Compose-Files für Broker ergänzt

---
+++ quarkus-kafka/quarkus-kafka-recv/src/main/java/de/gedoplan/showcase/service/KafkaRebalanceLogger.java
@@ -0,0 +1,35 @@
+package de.gedoplan.showcase.service;
+
+import java.util.Collection;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.common.TopicPartition;
+import org.jboss.logging.Logger;
+
+import io.smallrye.common.annotation.Identifier;
+import io.smallrye.reactive.messaging.kafka.KafkaConsumerRebalanceListener;
+
+@ApplicationScoped
+@Identifier("rebalance.logger")
+public class KafkaRebalanceLogger implements KafkaConsumerRebalanceListener  {
+    @Inject
+    Logger logger;
+
+    @Override
+    public void onPartitionsAssigned(Consumer<?, ?> consumer, Collection<TopicPartition> partitions) {
+        partitions.forEach(p -> logger.debugf("Assigned %s at position %d", p, consumer.position(p)));
+    }
+
+    @Override
+    public void onPartitionsLost(Consumer<?, ?> consumer, Collection<TopicPartition> partitions) {
+        partitions.forEach(p -> logger.debugf("Lost %s", p));
+    }
+
+    @Override
+    public void onPartitionsRevoked(Consumer<?, ?> consumer, Collection<TopicPartition> partitions) {
+        partitions.forEach(p -> logger.debugf("Revoked %s", p));
+    }
+}
--- quarkus-kafka/quarkus-kafka-recv/src/main/java/de/gedoplan/showcase/service/PersonReceiver.java
+++ quarkus-kafka/quarkus-kafka-recv/src/main/java/de/gedoplan/showcase/service/PersonReceiver.java
@@ -3,6 +3,7 @@ package de.gedoplan.showcase.service;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
 
@@ -15,7 +16,13 @@ public class PersonReceiver {
   Logger logger;
 
   @Incoming("person")
-  public void onReceive(Person person) {
-    this.logger.debugf("Received %s", person);
+  // public void onReceive(Person person) {
+  //   this.logger.debugf("Received %s", person);
+  // }
+  public void onReceive(ConsumerRecord<String, Person> record) {
+    Person person = record.value();
+    int partition = record.partition();
+    long offset = record.offset();
+    this.logger.debugf("Received %s (partition %d, offset %d)", person, partition, offset);
   }
 }
